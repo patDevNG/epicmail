383b0bf916636c80ad3df0dfbfd90e8df2dab809
patDev <cgrleadership@gmail.com>
2019-03-01 22:50:02 +0100

    initial commit

---
+++ dist/classes.js
@@ -0,0 +1,18 @@
+class Login {
+    constructor(email, password) {
+        this.email = email;
+        this.password = password;
+    }
+}
+class Register {
+    constructor(firstName, lastName, country, email, phone, gender, password, cnfpassword) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.country = country;
+        this.email = email;
+        this.phone = phone;
+        this.gender = gender;
+        this.password = password;
+        this.cnfpassword = cnfpassword;
+    }
+}
---
+++ dist/ui.js
@@ -0,0 +1,35 @@
+class UI {
+    constructor() {
+        // this.loginEmail = document.getElementById('loginEmail');
+        // this.loginPassword = document.getElementById('loginPassword');
+        // this.loginSubmit = document.getElementById('loginSubmit');
+
+    }
+    // show alert message
+    showAlert(message, className) {
+        // clear any remaining alerts
+        this.clearAlert();
+        const div = document.createElement('div');
+        // add classes
+        div.className = className;
+        // add text
+        div.appendChild(document.createTextNode(message));
+        // append it to the dom 
+        // get a parent
+        const card = document.querySelector('.card');
+        const cardBody = document.querySelector('.card-body');
+        // insert alert
+        card.insertBefore(div, cardBody);
+        // timeout after 3 secs
+        setTimeout(() => {
+            this.clearAlert()
+        }, 2000)
+    }
+    // clear alert message
+    clearAlert() {
+        const currentAlert = document.querySelector('.alert');
+        if (currentAlert) {
+            currentAlert.remove();
+        }
+    }
+}
